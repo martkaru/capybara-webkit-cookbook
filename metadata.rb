ba11f67fe22e9341c8cfd1befe8731de3d02d63f
Mart Karu <karu@metal.ee>
2014-04-16 20:56:40 +0300

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "sysadmin@cramerdev.com"
 license          "Apache 2.0"
 description      "Installs/Configures capybara-webkit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 supports         "ubuntu"
